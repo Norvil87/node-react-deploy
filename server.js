e40684873925851ca44906588c9a39e663adcc71
stepanov <stepanov@alfasystem.ru>
2020-12-24 15:50:18 +0300

    -

--- server.js
+++ server.js
@@ -1,38 +1,50 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+// Import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
+// Create a new express application called 'app'
 const app = express();
+
+// Set our backend port to be either an environment variable or port 5000
 const port = process.env.PORT || 5000;
 
+// This application level middleware prints incoming requests to the servers console
 app.use((req, res, next) => {
-  console.log(`Request_Endpoint: ${req.method} ${req.url}`);
-  next();
+    console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+    next();
 });
 
+// Set up the bodyParser middleware
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+// Set up the CORs middleware
 app.use(cors());
 
-const api = require("./routes/routes");
+// Require Route
+const api = require('./routes/routes');
+// Configure app to use route
+app.use('/api/v1/', api);
 
-app.use("/api/v1/", api);
+// This middleware informs the express application to serve our compiled React files
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+};
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
-
-app.get("*", (req, res) => {
-  res.status(200).json({ msg: "Catch All Response" });
+// Catch any bad requests
+app.get('*', (req, res) => {
+    res.status(200).json({
+        msg: 'Catch All'
+    });
 });
 
+// Set our server to listen on the port defiend by our port variable
 app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`));
